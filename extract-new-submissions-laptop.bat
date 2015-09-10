f51d1e8177512afd752e6558e2fec05f0a176eb6
Wang Kun <woshibendan2018@hotmail.com>
2015-09-10 20:58:57 +0800

    new submissions. For archive.

---
+++ extract-new-submissions-laptop.bat
@@ -0,0 +1,3 @@
+"D:\Program Files\Java\jdk1.8.0_25\bin\java.exe" -jar "D:\workspaces\repositories\arxiv-contents\arxiv-manager.jar"  -e -n -f
+
+
--- extract-new-submissions.bat
+++
@@ -1,3 +0,0 @@
-"C:\Program Files\Java\jdk7\bin\java.exe" -jar "D:\workspaces\arxiv\arxiv-manager.jar"  -e -n -f
-
-
